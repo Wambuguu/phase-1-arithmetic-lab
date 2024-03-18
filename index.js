108c654a98c148d641e66b5090fe0e01b1ee5a7d
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-03-18 16:21:57 +0300

    add

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let num3 = 20
+let num4 = 16
+let mod = num3 % num4
+let max = Math.max(2, 4, 6, 8, 20)
+
